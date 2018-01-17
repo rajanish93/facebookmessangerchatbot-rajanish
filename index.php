448e586e5ad47c41131e2c7c2f85db1116b85115
rajanish93 <singhajanish31@gmail.com>
2018-01-17 12:53:48 +0530

    modify index.pgp and add composer and vendore folder

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
-$access_token = "EAAay2RMWI3sBAIZCjSg9lVOJzSx0ZCrjAbxiWXtUPrEWUSZCRGoIiEWplegMswuux3gLQHuwrBZAqb8TMAZAvW2IZC8uHZCy0zFrwnR45aloLvVcdj2nW7DhaJiZCgPn9QHsvdaRiEPMWSCKaZBRnUrEFBlnQZCLyPEDygye8teQ4A9mKk18z7dAH5";
-$verify_token = "12132dffgfg";
-$hub_verify_token = null;
+require__DIR__.'vendor/autoload.php';
+use Mpociot\BotMan\BotManFactory;
+use Mpociot\BotMan\BotMan;
+use Mpociot\BotMan\Cache\DoctrineCache;
 
-if(isset($_REQUEST['hub_challenge'])) {
-    $challenge = $_REQUEST['hub_challenge'];
-    $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
+$botman = BotManFactory::create($config, new DoctrineCache($doctrineCacheDriver));
 
+$config = [
+    'hipchat_urls' => [
+        'YOUR-INTEGRATION-URL-1',
+        'YOUR-INTEGRATION-URL-2',
+    ],
+    'nexmo_key' => 'YOUR-NEXMO-APP-KEY',
+    'nexmo_secret' => 'YOUR-NEXMO-APP-SECRET',
+    'microsoft_bot_handle' => 'YOUR-MICROSOFT-BOT-HANDLE',
+    'microsoft_app_id' => 'YOUR-MICROSOFT-APP-ID',
+    'microsoft_app_key' => 'YOUR-MICROSOFT-APP-KEY',
+    'slack_token' => 'YOUR-SLACK-TOKEN-HERE',
+    'telegram_token' => 'YOUR-TELEGRAM-TOKEN-HERE',
+    'facebook_token' => 'EAAay2RMWI3sBAIZCjSg9lVOJzSx0ZCrjAbxiWXtUPrEWUSZCRGoIiEWplegMswuux3gLQHuwrBZAqb8TMAZAvW2IZC8uHZCy0zFrwnR45aloLvVcdj2nW7DhaJiZCgPn9QHsvdaRiEPMWSCKaZBRnUrEFBlnQZCLyPEDygye8teQ4A9mKk18z7dAH5',
+    'facebook_app_secret' => 'YOUR-FACEBOOK-APP-SECRET-HERE',
+    'wechat_app_id' => 'YOUR-WECHAT-APP-ID',
+    'wechat_app_key' => 'YOUR-WECHAT-APP-KEY',
+];
 
-if ($hub_verify_token === $verify_token) {
-    echo $challenge;
-}
-?>
+// create an instance
+$botman = BotManFactory::create($config);
+
+$botman->verifyServices('12132dffgfg');
+// give the bot something to listen for.
+$botman->hears('hello', function (BotMan $bot) {
+    $bot->reply('Hello yourself.');
+});
+
+// start listening
+$botman->listen();
+
